a62b90745a378d4dc9e1c842ecbb807573109413
Diana Marcelin <dianamarcelin@Dianas-MacBook-Air.local>
2022-01-28 08:24:45 -0500

    querying a collection

--- index.js
+++ index.js
@@ -27,13 +27,44 @@ initializeApp({
 const db = getFirestore();
 
 //create a collection called "restaurants"
+const restRef = db.collection("restaurants");
 
-//add each restaurant
-db.collection("restaurants")
-  .add(restaurants[0])
-  .then((doc) => {
-    console.log("Added restaurant", doc.id);
+// // //add each restaurant >>> db.---go into firestore and....
+// restRef
+//   .add(restaurants[3])
+//   .then((doc) => {
+//     console.log("Added restaurant", doc.id);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+//read one document---go into collection and get a single doc
+// restRef
+//   .doc("zGeeMU6247M2AWbadu8m")
+//   .get()
+//   .then((doc) => {
+//     console.log(doc.id, " =>", doc.data());
+//   })
+//   .catch((err) => console.error(err));
+
+//get all documents
+// restRef
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, " => ", doc.data());
+//     });
+//   })
+//   .catch(console.error);
+
+//querying a collection >> find a document(s)---->collection of Bolay's---dont know id but want to get a specific rest
+restRef
+  .where("name", "==", "McDonald's")
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
+    });
   })
-  .catch((err) => {
-    console.error(err);
-  });
+  .catch(console.error);
